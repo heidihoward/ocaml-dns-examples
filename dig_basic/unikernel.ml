1396f49a7a896f9fd4a925032c9d7d7b2f7f10bb
Heidi-ann <hh360@cam.ac.uk>
2015-02-17 12:23:14 +0000

    simplify further

--- dig_basic/unikernel.ml
+++ dig_basic/unikernel.ml
@@ -12,15 +12,15 @@ module Client (C:CONSOLE) (S:STACKV4) = struct
 
   let start c s =
     let t = DNS.create s in
-    C.log_s c "Started, will begin resolving shortly..." >>= fun () ->
-    OS.Time.sleep 2.0 >>= fun () ->
-      C.log_s c ("Resolving " ^ domain)
-      >>= fun () ->
-      DNS.gethostbyname t ~server "google.com"
-      >>= fun rl ->
-      Lwt_list.iter_s
-        (fun r ->
-           C.log_s c ("Answer " ^ (Ipaddr.to_string r))
-        ) rl
+    C.log_s c "Started, will begin resolving shortly..." 
+    >>= fun () ->
+    C.log_s c ("Resolving " ^ domain)
+    >>= fun () ->
+    DNS.gethostbyname t ~server "google.com"
+    >>= fun rl ->
+    Lwt_list.iter_s
+      (fun r ->
+         C.log_s c ("Answer " ^ (Ipaddr.to_string r))
+      ) rl
 
 end
