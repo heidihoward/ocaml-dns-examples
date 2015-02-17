0d39eeba258122b747973dcf4cf2dbbbcf8933d8
Heidi-ann <hh360@cam.ac.uk>
2015-02-17 12:25:31 +0000

    readding the sleep

--- dig_basic/unikernel.ml
+++ dig_basic/unikernel.ml
@@ -12,7 +12,7 @@ module Client (C:CONSOLE) (S:STACKV4) = struct
 
   let start c s =
     let t = DNS.create s in
-    C.log_s c "Started, will begin resolving shortly..." 
+    OS.Time.sleep 2.0 
     >>= fun () ->
     C.log_s c ("Resolving " ^ domain)
     >>= fun () ->
@@ -20,7 +20,7 @@ module Client (C:CONSOLE) (S:STACKV4) = struct
     >>= fun rl ->
     Lwt_list.iter_s
       (fun r ->
-         C.log_s c ("Answer " ^ (Ipaddr.to_string r))
+         C.log_s c ("  => " ^ (Ipaddr.to_string r))
       ) rl
 
 end
