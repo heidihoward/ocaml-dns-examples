734aaf454a66099c33b4ac5ddae19a472765b249
Heidi-ann <hh360@cam.ac.uk>
2015-02-17 12:10:24 +0000

    fixing up strings

--- dig_basic/unikernel.ml
+++ dig_basic/unikernel.ml
@@ -12,16 +12,16 @@ module Client (C:CONSOLE) (S:STACKV4) = struct
 
   let start c s =
     let t = DNS.create s in
-    C.log_s c "Started, will begin resolving shortly..." >>= fun () ->
+    C.log c "Started, will begin resolving shortly..." >>= fun () ->
     OS.Time.sleep 2.0 >>= fun () ->
     while_lwt true do
-      C.log_s c ("Resolving %s" domain)
+      C.log c ("Resolving " ^ domain)
       >>= fun () ->
       DNS.gethostbyname t ~server "google.com"
       >>= fun rl ->
       Lwt_list.iter_s
         (fun r ->
-           C.log_s c ("  => %s" (Ipaddr.to_string r))
+           C.log c ("  => " ^ (Ipaddr.to_string r))
         ) rl
       >>= fun () ->
       OS.Time.sleep 1.0
