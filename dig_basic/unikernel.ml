eb9523f4f8cd5a3300810d39908ce353c3d221e7
Heidi-ann <hh360@cam.ac.uk>
2015-02-17 12:06:00 +0000

    adding basic example from ocaml-dns

---
+++ dig_basic/unikernel.ml
@@ -0,0 +1,29 @@
+open Lwt
+open V1_LWT
+open Printf
+
+let domain = "google.com"
+let server = Ipaddr.make 8 8 8 8
+
+module Client (C:CONSOLE) (S:STACKV4) = struct
+
+  module U = S.UDPV4
+  module DNS = Dns_resolver_mirage.Make(OS.Time)(S)
+
+  let start c s =
+    let t = DNS.create s in
+    C.log_s c "Started, will begin resolving shortly..." >>= fun () ->
+    OS.Time.sleep 2.0 >>= fun () ->
+    while_lwt true do
+      C.log_s c (green "Resolving %s" domain)
+      >>= fun () ->
+      DNS.gethostbyname t ~server "google.com"
+      >>= fun rl ->
+      Lwt_list.iter_s
+        (fun r ->
+           C.log_s c (yellow "  => %s" (Ipaddr.to_string r))
+        ) rl
+      >>= fun () ->
+      OS.Time.sleep 1.0
+    done
+end
