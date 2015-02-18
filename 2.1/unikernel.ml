245d2c81d0f7e92e2641111496c0c888fcf5f947
Heidi-ann <hh360@cam.ac.uk>
2015-02-18 16:59:31 +0000

    updating part 1 for tutorial

---
+++ 2.1/unikernel.ml
@@ -0,0 +1,35 @@
+open Lwt
+open V1_LWT
+
+module Client (C:CONSOLE) (S:STACKV4) = struct
+
+  module U = S.UDPV4
+  module DNS = Dns_resolver_mirage.Make(OS.Time)(S)
+
+  open Dns.Packet
+  open Dns.Name
+
+  let start c s =
+    Bootvar.create () >>= fun bootvar ->
+    let domain = Bootvar.get_exn bootvar "domain" in
+    let server = Ipaddr.V4.of_string_exn (Bootvar.get_exn bootvar "server") in
+    let t = DNS.create s in
+    OS.Time.sleep 2.0 
+    >>= fun () ->
+    C.log_s c ("Resolving " ^ domain)
+    >>= fun () ->
+    DNS.resolve (module Dns.Protocol.Client) t server 53 Q_IN Q_A (string_to_domain_name domain)
+    >>= fun r ->
+    let ips =
+    List.fold_left (fun a x ->
+      match x.rdata with
+      | A ip -> (Ipaddr.V4 ip) :: a
+      | _ -> a ) [] r.answers in
+    Lwt_list.iter_s
+      (fun r ->
+         C.log_s c ("Answer " ^ (Ipaddr.to_string r))
+      ) ips
+    >>= fun () ->
+        C.log_s c (to_string r)
+
+end
