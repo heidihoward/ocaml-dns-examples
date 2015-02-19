d5247c6142529e951cef583871c2cd0839da9370
Heidi-ann <hh360@cam.ac.uk>
2015-02-19 15:40:35 +0000

    trying new config file

--- 2.1/config.ml
+++ 2.1/config.ml
@@ -2,15 +2,20 @@ open Mirage
 
 let data = crunch "./data"
 
+let handler =
+  foreign "Unikernel.Main" (console @-> kv_ro @-> stackv4 @-> job)
+
 let ip_config:ipv4_config = {
   address= Ipaddr.V4.make 192 168 1 2;
   netmask= Ipaddr.V4.make 255 255 255 0;
   gateways= [Ipaddr.V4.make 192 168 1 1];
 }
 
-let client =
-  foreign "Unikernel.Client" @@ console @-> kv_ro @-> stackv4 @-> job
+let direct =
+  let stack = direct_stackv4_with_static_ipv4 default_console tap0 ip_config  in
+  handler $ default_console $ data $ stack
 
 let () =
-  add_to_ocamlfind_libraries [ "dns.mirage"; ];
-  register "dns-client" [ client $ default_console $ data $ direct_stackv4_with_static_ipv4 default_console tap0 ip_config]
+  add_to_ocamlfind_libraries ["dns.lwt-core"];
+  add_to_opam_packages ["dns"];
+  register "dns" [direct]
