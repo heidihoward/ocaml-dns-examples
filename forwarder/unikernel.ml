1fa3bd9102eba9c515d8e1baf1c10ae9ee5ae40a
Heidi-ann <hh360@cam.ac.uk>
2015-03-14 13:28:47 +0000

    adding forwarder example

---
+++ forwarder/unikernel.ml
@@ -0,0 +1,35 @@
+open Lwt
+open V1_LWT
+open Dns
+open Dns_server
+
+let port = 53
+let zonefile = "test.zone"
+let resolver_addr = Ipaddr.V4.make 8 8 8 8
+let resolver_port = 53
+
+module Main (C:CONSOLE) (K:KV_RO) (S:STACKV4) = struct
+
+  module U = S.UDPV4
+  module DS = Dns_server_mirage.Make(K)(S)
+  module DR = Dns_resolver_mirage.Make(OS.Time)(S)
+
+  let process resolver c ~src ~dst packet =
+      let open Packet in
+      C.log_s c (to_string packet) 
+      >>= fun () ->
+      match packet.questions with
+      | [] -> return None (* no questions in packet *)
+      | [q] -> 
+            DR.resolve (module Dns.Protocol.Client) resolver resolver_addr resolver_port q.q_class q.q_type q.q_name 
+            >>= fun result ->
+            (C.log_s c (to_string result) >>= fun () ->
+            return (Some (Dns.Query.answer_of_response result))) 
+      | _ -> return None (* we are not supporting QDCOUNT > 1 *)
+
+  let start c k s =
+    let server = DS.create s k in
+    let resolver = DR.create s in
+    let processor = ((Dns_server.processor_of_process (process resolver c)) :> (module Dns_server.PROCESSOR)) in 
+    DS.serve_with_processor server ~port ~processor
+end
