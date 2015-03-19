bade971e9559a8c22aaa891993d6ba72c5f1fa6e
Heidi-ann <hh360@cam.ac.uk>
2015-03-19 13:41:09 +0000

    next attempt at using travis

--- travis.sh
+++ travis.sh
@@ -1,19 +1,8 @@
 #!/usr/bin/env bash
 
-# generic mirage/opam setup
-echo "yes" | sudo add-apt-repository ppa:avsm/ocaml42+opam12
-sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam 
 
-export OPAMYES=1
-opam init
-opam install mirage dns
-eval `opam config env`
-
-# project specific
 for dir in * ; do
   cd $dir
   mirage configure --unix
   make
-  sudo make run
 done
